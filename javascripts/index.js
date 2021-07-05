589cdedb68dd76bd4f5af57010ce4d4ca8bd916b
codymerritt347 <cody.merritt@icloud.com>
2021-07-05 18:56:29 -0400

    Create exercise.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,22 +1,33 @@
-const buttonDiv = () => document.getElementById('button-control');
-const button = () => document.getElementById('button');
-const ulDiv = () => document.getElementById('list');
-const ul = () => document.getElementById('workouts-list');
+const wecomeContainer = () => document.getElementById('welcome-container');
+const myWorkoutsButton = () => document.getElementById('my-workouts-button');
+const newWorkoutButton = () => document.getElementById('new-workout-button');
+const myWorkoutsContainer = () => document.getElementById('my-workouts-container');
+const myWorkoutsList = () => document.getElementById('my-workouts-list');
 
 document.addEventListener('DOMContentLoaded', () => {
-  button().addEventListener('click', handleClick)
+  myWorkoutsButton().addEventListener('click', myWorkoutsClick);
 })
 
-const handleClick = () => {
-  fetch('http://localhost:3000/workouts')
-  .then(resp => resp.json())
-  .then(json => renderWorkouts(json))
-  .catch(handleError)
+const myWorkoutsClick = () => {
+  if (myWorkoutsList().children.length < 1) {
+    fetch('http://localhost:3000/workouts')
+    .then(resp => resp.json())
+    .then(json => renderWorkouts(json))
+    .catch(handleError)
+  } else {
+    myWorkoutsList().innerHTML = ''
+  }
 }
 
 const renderWorkouts = (workouts) => {
   workouts.forEach(workout => {
-    const li = document.createElement('li')
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <h2 class='workout-name'>${workout.day}</h2>
+      <h4 class='workout-date'>${workout.length}</h4>
+      <h4 class='workout-date'>${workout.where}</h4>
+    `;
+    myWorkoutsList().appendChild(li);
   })
 }
 
